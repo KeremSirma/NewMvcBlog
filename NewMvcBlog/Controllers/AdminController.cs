a7463e0f98c1a94efd5967e1fd3a9cd0e397a548
Kerem Sırma <kerem.sirma@hotmail.com>
2017-06-17 16:55:07 -1200

    Categories

---
+++ NewMvcBlog/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewMvcBlog.Models;
+
+namespace NewMvcBlog.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        MVCBlogDb db = new MVCBlogDb();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+
+        public ActionResult KategoriPartial()
+        {
+            return View(db.Kategoris.ToList());
+        }
+    }
+}
---
+++ NewMvcBlog/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewMvcBlog.Models;
+
+namespace NewMvcBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCBlogDb db = new MVCBlogDb();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+
+        public ActionResult KategoriPartial()
+        {
+            return View(db.Kategoris.ToList());
+        }
+    }
+}
